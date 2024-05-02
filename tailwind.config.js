ed89ec6af5ea9dc92dc15de28216fbb1976175e9
Mtmuhammad <marcellus.muhammad@yahoo.com>
2024-05-01 22:29:50 -0400

    feat: Add Html
    Added html and tailwindcss classes.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,20 @@
 module.exports = {
   content: ["./build/**/*.{html,js}"],
   theme: {
+    colors: {
+      'nutmeg': 'hsl(14, 45%, 36%)',
+      'dark-raspberry': 'hsl(332, 51%, 32%)',
+      'white': 'hsl(0, 0%, 100%)',
+      'rose-white': 'hsl(330, 100%, 98%)',
+      'eggshell': ' hsl(30, 54%, 90%)',
+      'light-grey': 'hsl(30, 18%, 87%)',
+      'wenge-brown': 'hsl(30, 10%, 34%)',
+      'dark-charcoal': 'hsl(24, 5%, 18%)',
+    },
+    fontFamily: {
+      outfit: ['Outfit', 'sans-serif'],
+      serif: ['Young Serif', 'serif'],
+    },
     extend: {},
   },
   plugins: [],
